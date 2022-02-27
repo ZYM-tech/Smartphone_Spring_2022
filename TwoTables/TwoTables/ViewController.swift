344214b8cf96a2780f1419ca79a26a417ebf57d2
ZYM <yiming.zhang1024@gmail.com>
2022-02-27 11:02:13 -0800

    editingStyle 删除功能

--- TwoTables/TwoTables/ViewController.swift
+++ TwoTables/TwoTables/ViewController.swift
@@ -42,5 +42,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             return cell
         }
     }
+    
+    //添加删除功能：editingStyle
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if tableView == tbl1{
+                arr1.remove(at: indexPath.row)
+            }else {
+                arr2.remove(at: indexPath.row)
+            }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
 }
 
